a1ee4d8ce871f8d4b85cc002a130e2a1bb3fb680
lapoiz <poizatd@hotmail.com>
2019-12-03 23:00:59 +0100

    Ajout liste spots dans Nav Bar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 
 use App\Repository\SpotRepository;
+use App\Service\DisplayObject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,11 +16,12 @@ class HomeController  extends AbstractController
      * @Route("/" , name="home")
      * @return Response
      */
-    public function index(SpotRepository $repository) : Response
+    public function index(SpotRepository $repository, DisplayObject $displayObject) : Response
     {
         $spots = $repository->findAll();
         return $this->render('pages/Home.html.twig',[
             "spots" => $spots,
+            "regionsNavBar" => $displayObject->regionsForNavBar($spots),
             "urlImage" => $this->getParameter('card_image_directory')
         ]);
     }
